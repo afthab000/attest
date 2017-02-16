bbc22ab3edb80610534836e1341caa369cdba76e
DESKTOP-J9B8F63\Afthab <afthab000@gmail.com>
2017-02-16 13:59:16 +0530

    yeoman test

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testangApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the testangApp
+ */
+angular.module('testangApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
--- public/js/admin-home_controller.js
+++
@@ -1,9 +0,0 @@
-app.controller('adminHomeCtrl', ['$scope', '$http', function ($scope, $http) {
-    
-
-    $http.get('./files/view').then(function(response){
-        $scope.currentFiles =response.data;
-    });
-    
-   
-}]);
