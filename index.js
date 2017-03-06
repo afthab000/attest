60129a809bff46f5b37dce90524b9d249830ca3f
DESKTOP-J9B8F63\Afthab <afthab000@gmail.com>
2017-03-06 19:02:54 +0530

    no message

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-﻿require('rootpath')();
+require('rootpath')();
 var express = require('express');
 var app = express();
 var session = require('express-session');
@@ -14,7 +14,9 @@ app.use(session({ secret: config.secret, resave: false, saveUninitialized: true 
 
 // use JWT auth to secure the api
 app.use('/api', expressJwt({ secret: config.secret }).unless({ path: ['/api/users/authenticate', '/api/users/register'] }));
-
+app.get('/', function (req, res) {
+    return res.redirect('/app');
+});
 // routes
 app.use('/login', require('./controllers/login.controller'));
 app.use('/register', require('./controllers/register.controller'));
@@ -22,11 +24,9 @@ app.use('/app', require('./controllers/app.controller'));
 app.use('/api/users', require('./controllers/api/users.controller'));
 
 // make '/app' default route
-app.get('/', function (req, res) {
-    return res.redirect('/app');
-});
+
 
 // start server
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 5000, function () {
     console.log('Server listening at http://' + server.address().address + ':' + server.address().port);
 });
